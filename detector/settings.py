5a55bfc61acfd21619f07a7456564d7f7fa45af8
Team Status <gleseur+status@work4labs.com>
2015-01-28 15:47:06 +0000

    adding id to connect meteor

--- detector/settings.py
+++ detector/settings.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals
 
 DETECTION_PAIRS = {
     "male": {
+        "id": 1,  # To communicate with Meteor API
         "pir": 7,
         "light": 17,
         "free_time": 5,  # time in seconds after which we deem the room freed
