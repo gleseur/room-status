7d760246fe8dd8bd5c24b59a45d607bf0a89f07c
Guillaume Leseur <gleseur@work4labs.com>
2015-01-28 18:07:06 +0100

    Only filling db if empty

--- room-status/startup.js
+++ room-status/startup.js
@@ -1,24 +1,26 @@
 if (Meteor.isServer) {
     Meteor.startup(function () {
-        Room.remove({});
-        BusyTime.remove({});
-        Room.insert({
-            room_id: 1,
-            name: "man"
-        });
-        Room.insert({
-            room_id: 2,
-            name: "woman"
-        });
-        BusyTime.insert({
-            room_id: 1,
-            opened_at: moment().toDate(),
-            status: "free"
-        });
-        BusyTime.insert({
-            room_id: 2,
-            opened_at: moment().toDate(),
-            status: "free"
-        });
+        if (Room.find().count() == 0) {
+            Room.insert({
+                room_id: 1,
+                name: "man"
+            });
+            Room.insert({
+                room_id: 2,
+                name: "woman"
+            });
+        }
+        if (BusyTime.find().count() == 0) {
+            BusyTime.insert({
+                room_id: 1,
+                opened_at: moment().toDate(),
+                status: "free"
+            });
+            BusyTime.insert({
+                room_id: 2,
+                opened_at: moment().toDate(),
+                status: "free"
+            });
+        }
     });
 }
