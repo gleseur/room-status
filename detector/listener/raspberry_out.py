09d20f9f50f7e296ec1cab38d6d6d31ff9b68632
Benjamin Combourieu <bcombourieu@work4labs.com>
2015-01-28 16:37:04 +0100

    Subscribing to busy computer actions

--- detector/listener/raspberry_out.py
+++ detector/listener/raspberry_out.py
@@ -11,6 +11,15 @@ def motion_action(sender):
 def idle_action(sender):
     print "Back to idle state {}".format(sender.name)
 
+def busy_action(sender):
+    print "Action detected in the bathroom {}".format(sender.name)
+
+def room_freed_action(sender):
+    print "Room {} has been freed. Bye Bye".format(sender.name)
+
+
 signals.setup.connect(setup_action)
 signals.motion.connect(motion_action)
 signals.idle.connect(idle_action)
+signals.busy.connect(busy_action)
+signals.room_freed.connect(room_freed_action)
